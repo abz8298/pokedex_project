20e5d452f6787596f1b8b5954c488e6d47f02a08
M Arbaz <mdarbaz8298@gmail.com>
2024-09-04 17:06:18 +0530

    basic pokedex setup done!

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,12 @@
+import '../Search/Search.css';
+
+
+function Search(){
+    return(
+        <div className='search-wrapper'>
+            <input type="text" placeholder="pokemon name..." id='pokemon-name-search'/>
+        </div>
+    )
+}
+
+export default Search;
